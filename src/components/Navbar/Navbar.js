82b2b6d49a747c1e0c8fa424d3004392dcec6f2d
Daniel Fajt <daniel29se@gmail.com>
2020-08-22 23:17:25 -0700

    navbar style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
+import { AiFillHome } from "react-icons/ai";
+import { AiFillIdcard } from "react-icons/ai";
+import { AiFillFolderOpen } from "react-icons/ai";
+import { AiFillWechat } from "react-icons/ai";
 
 function Navbar() {
   return (
     <nav className="nav">
-      <span className="nav__logo">Daniel Fajt</span>
       <ul className="nav__links">
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <AiFillHome className="nav__icon" />
+            <span>Home</span>
+          </Link>
         </li>
 
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about">
+            <AiFillIdcard className="nav__icon" />
+            About
+          </Link>
         </li>
 
         <li>
-          <Link to="/projects">Projects</Link>
+          <Link to="/projects">
+            <AiFillFolderOpen className="nav__icon" />
+            Projects
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/projects">
+            <AiFillWechat className="nav__icon" />
+            Contact
+          </Link>
         </li>
       </ul>
-      <span>
-        <button className="nav__contact">
-          <Link to="/contact">Contact</Link>
-        </button>
-      </span>
     </nav>
   );
 }
