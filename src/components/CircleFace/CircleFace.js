41b600f775b3c645b0f6a180fc2515bc19e42348
Daniel Fajt <daniel29se@gmail.com>
2020-09-09 12:16:54 -0700

    content layout CSS bg color, instead layout bg

---
+++ src/components/CircleFace/CircleFace.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CircleFace.css";
+
+function CircleFace() {
+  return <div className="circleface"></div>;
+}
+
+export default CircleFace;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,18 @@
 import React from "react";
+// Styles
+import "../styles/About.css";
+// Components
+import CircleFace from "../components/CircleFace/CircleFace";
+// Static data
+import { aboutMe } from "../assets/_staticData";
 
 function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="about">
+      <div>
+        <CircleFace />
+      </div>
+      <div className="abouttext">{aboutMe}</div>
     </div>
   );
 }
